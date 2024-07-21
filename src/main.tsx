e03787292e46571fe52f1cd475e6092438561c69
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-21 11:09:04 +0200

    Install + setup react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import GlobalStyles from "./GlobalStyles.ts";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <Router>
+      <GlobalStyles />
+      <App />
+    </Router>
   </React.StrictMode>
 );
