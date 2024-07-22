51c478f41420eefabde7a3f0198570056cccecea
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-22 19:46:13 +0200

    Update MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,17 +1,22 @@
 import { Header } from "../features/main-page/Header";
 import { FactCard } from "../features/main-page/FactCard";
 import styled from "styled-components";
+import { SideBar } from "../features/main-page/SideBar";
 
 const StyledMainPage = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, 10vw, 1fr);
-  width: 90vw;
+  grid-template-columns: 21rem minmax(50rem, 1fr);
+  align-items: start;
+
+  width: 80vw;
+  padding: 3.2rem 0;
 `;
 
 export const MainPage = () => {
   return (
     <StyledMainPage>
       <Header />
+      <SideBar />
       <FactCard />
     </StyledMainPage>
   );
