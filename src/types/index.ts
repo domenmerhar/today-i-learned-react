3445b0ae4dcd80ebc96960bbe32c04b777efaad9
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-22 10:45:16 +0200

    Add types/index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type BackgroundColorType =
+  | "blue"
+  | "green"
+  | "red"
+  | "yellow"
+  | "pink"
+  | "teal"
+  | "orange"
+  | "purple"
+  | "gradient";
+
+export type BadgeType =
+  | "technology"
+  | "science"
+  | "finance"
+  | "society"
+  | "entertainment"
+  | "health"
+  | "history"
+  | "news";
