903c51f00ae63434080d83dfa079e0355d4e8bbf
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-22 20:23:59 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import GlobalStyles from "./GlobalStyles.ts";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
+      <Toaster
+        position="top-center"
+        toastOptions={{ duration: 5000 }}
+        containerStyle={{ fontSize: "1.6rem" }}
+      />
       <GlobalStyles />
       <App />
     </Router>
