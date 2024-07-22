3bfb70177463c1611a2d052e4e23de9ea36a99dc
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-22 10:45:04 +0200

    Update Header.tsx

--- src/features/main-page/Header.tsx
+++ src/features/main-page/Header.tsx
@@ -2,21 +2,30 @@ import styled from "styled-components";
 import { Logo } from "../../Util/Logo";
 import { Button } from "../../Util/Button";
 import { AddFact } from "./AddFact";
+import { useState } from "react";
 
 const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  grid-column: 1 / -1;
 `;
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => setIsOpen((prev) => !prev);
+
   return (
     <>
       <StyledHeader>
         <Logo />
-        <Button backgroundColor="gradient">Share a fact</Button>
+        <Button backgroundColor="gradient" onClick={handleClick}>
+          {isOpen ? "Close" : "Share a fact"}
+        </Button>
       </StyledHeader>
-      <AddFact />
+      {isOpen && <AddFact />}
     </>
   );
 };
