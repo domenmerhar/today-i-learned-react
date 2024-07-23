7688cb671c8cbb8f83100bbe88db8f7bce27be79
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-23 20:10:14 +0200

    Update FactList.tsx

--- src/features/main-page/FactList.tsx
+++ src/features/main-page/FactList.tsx
@@ -3,11 +3,20 @@ import { FactCard } from "./FactCard";
 import { useSearchParams } from "react-router-dom";
 import { useFacts } from "../../hooks/useFacts";
 import { BadgeType } from "../../types";
+import { PuffLoader } from "react-spinners";
 
 const StyledFactList = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
+`;
+
+const LoadingBarHolder = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
 `;
 
 export const FactList = () => {
@@ -17,7 +26,12 @@ export const FactList = () => {
 
   if (error) return <div>error</div>;
 
-  if (isLoading || !data) return <div>loading</div>;
+  if (isLoading || !data)
+    return (
+      <LoadingBarHolder>
+        <PuffLoader color="#d4d4d8" size={128} />
+      </LoadingBarHolder>
+    );
 
   return (
     <StyledFactList>
