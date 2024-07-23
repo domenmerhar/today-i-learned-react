c387dd4d0c3de8f995c53f33806d098c79c76ac3
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-23 10:03:06 +0200

    Update main.tsx; React-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,30 @@ import App from "./App.tsx";
 import GlobalStyles from "./GlobalStyles.ts";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <Toaster
         position="top-center"
         toastOptions={{ duration: 5000 }}
         containerStyle={{ fontSize: "1.6rem" }}
       />
-      <GlobalStyles />
-      <App />
-    </Router>
+      <Router>
+        <GlobalStyles />
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>
 );
