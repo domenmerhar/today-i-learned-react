a80b95c790f026558a2d0cb15010ffde75f06625
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-24 12:34:01 +0200

    Update MainPage.tsx; Styling

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,13 +5,30 @@ import { FactList } from "../features/main-page/FactList";
 
 const StyledMainPage = styled.div`
   display: grid;
-  grid-template-columns: 21rem minmax(50rem, 1fr);
+  grid-template-columns: 21rem minmax(1rem, 1fr);
+  grid-template-areas:
+    "header header"
+    "add-fact add-fact"
+    "sidebar content";
   align-items: start;
   gap: 1.6rem;
 
   width: 75vw;
   max-width: 1440px;
   padding: 3.2rem 0;
+
+  @media (max-width: 52em) {
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      "header"
+      "add-fact"
+      "sidebar"
+      "content";
+  }
+
+  @media (max-width: 34em) {
+    width: 85vw;
+  }
 `;
 
 export const MainPage = () => {
