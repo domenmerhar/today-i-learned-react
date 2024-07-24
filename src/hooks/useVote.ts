b086ba5936130724a9b055e3c807414a444da2ca
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-24 11:57:28 +0200

    Update useVote.ts

--- src/hooks/useVote.ts
+++ src/hooks/useVote.ts
@@ -1,11 +1,30 @@
-import { useMutation } from "@tanstack/react-query";
+import {
+  InvalidateQueryFilters,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { changeVote } from "../api/changeVote";
-import { ApiVoteType } from "../types";
+import { ApiVoteType, CategoryType } from "../types";
 
-export const useVote = (id: string, vote: ApiVoteType, add: boolean) => {
+export const useVote = (
+  id: string,
+  vote: ApiVoteType,
+  add: boolean,
+  category: Omit<CategoryType, "all">
+) => {
+  const queryClient = useQueryClient();
+
   const { data, error, mutate } = useMutation({
     mutationKey: ["vote", id, vote, add],
     mutationFn: () => changeVote(id, vote, add),
+    onSettled: () => {
+      console.log("Vote changed");
+      queryClient.invalidateQueries(["facts"] as InvalidateQueryFilters);
+      queryClient.invalidateQueries([
+        "facts",
+        category,
+      ] as InvalidateQueryFilters);
+    },
   });
 
   return { data, error, mutate };
